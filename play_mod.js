90ce8fb19bedb3e8b486f12c3c5abcaa735d9a53
Riboshom <riboshom@hotmail.com>
2017-04-27 15:38:22 -0400

    play_mod tempo and channel assignation

--- play_mod.js
+++ play_mod.js
@@ -1,3 +1,7 @@
+var {Routines, SteamControllerSequence, SteamControllerPlayer} = require("steam-controller-player");
+var {ArpeggioNote, FlatNote} = require("./sc_music.js");
+var StopRoutine = Routines.StopRoutine;
+
 const OpenMTP_Module = require('node-libopenmpt');
 var Promise = require("bluebird");
 const fs = require('fs');
@@ -9,20 +13,28 @@ var tmpFilePathVar = 'alf2_zalza_edit.xm';
 readFile(tmpFilePathVar).then(function(data) {
     return new OpenMTP_Module(data);
 }).then(function(module){
+	var sequence = new SteamControllerSequence();	
+	var channels = SteamControllerPlayer.channels;
+	var maxChannels = Math.max(module.num_channels, channels.length)
+	var sequenceCounter = 0;
+	
+	var tempo = module.current_tempo;
+	var speed = module.current_speed;
+	
 	for(var order = module.current_order; order < module.num_orders; order++) {
 		const pattern = module.get_order_pattern(order);
 		for(var row = 0; row < module.get_pattern_num_rows(pattern); row++) {
 			module.set_position_order_row(order, row);
-			var channelUpdates = [];
-			for(var channel = 0; channel < module.num_channels; channel++) {
-				channelUpdates.push(module.get_pattern_row_channel(pattern, row, channel))
+			
+			for(var channel = 0; channel < maxChannels; channel++) {
+				const update = module.get_pattern_row_channel(pattern, row, channel)
+				sequence.add(sequenceCounter, channels[channel], new FlatNote(update.note));
 			}
-			var rowString = "";
-			channelUpdates.forEach(function(update, idx){
-				if(idx !== 0) rowString += " | ";
-				rowString += update.string;
-			})
-			console.log(rowString)
+			sequenceCounter++;
 		}
 	}
-});
+	
+	// Using clasic tempo mode, see OpenMPT wiki
+	// https://wiki.openmpt.org/Manual:_Song_Properties#Tempo_Mode
+	return SteamControllerPlayer.playSequence(sequence, tempo, 24/speed, speed);
+})
